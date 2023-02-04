ab30163c73a275b0dd5f7e5711ac54a796cf7b86
Daniela Correa <dacomu24@gmail.com>
2023-02-04 12:55:41 -0500

    Game logic and some styling details

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let birdBottom = 100
     let gravity = 2
     let isGameOver = false
+    let gap = 430
 
     function starGame() {
         birdBottom -= gravity
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function jump() {
         if (birdBottom < 500) {
             birdBottom += 50
-            bird.style.bottom = `${birdLeft}px`
+            bird.style.bottom = `${birdBottom}px`
 
         }
 
@@ -39,36 +40,48 @@ document.addEventListener('DOMContentLoaded', () => {
         let randomHeight = Math.random() * 60
         let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
-        obstacle.classList.add('obstacle')
+        const topObstacle = document.createElement('div')
+        if (!isGameOver) {
+            obstacle.classList.add('obstacle')
+            topObstacle.classList.add('topObstacle')
+        }
         gameDisplay.appendChild(obstacle)
+        gameDisplay.appendChild(topObstacle)
         obstacle.style.left = `${obstacleLeft}px`
+        topObstacle.style.left = `${obstacleLeft}px`
         obstacle.style.bottom = `${obstacleBottom}px`
+        topObstacle.style.bottom = `${obstacleBottom + gap}px`
 
         function moveObstacle() {
             obstacleLeft -= 2
             obstacle.style.left = `${obstacleLeft}px`
+            topObstacle.style.left = `${obstacleLeft}px`
 
             if (obstacleLeft === -60) {
                 clearInterval(timerId)
                 gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
 
 
             }
             if (
-
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
+                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200) ||
                 birdBottom === 0) {
                 gameOver()
+                clearInterval(timerId)
             }
 
         }
 
         let timerId = setInterval(moveObstacle, 20)
-        setTimeout(generateObstacle, 3000)
+        if (!isGameOver) setTimeout(generateObstacle, 3000)
     }
     generateObstacle()
 
     function gameOver() {
         clearInterval(gameTimerId)
+        console.log("Game over");
         isGameOver = true
         document.removeEventListener('keyup', control)
     }
